8b96cd277e83b3fc785ba922e06de22a86c8e748
noelle <nrosenbe@hsz-t.ch>
2010-12-09 18:03:23 +0100

    Comboboxen des Fahreugdaten verwalten GUIs mit entsprechenden Daten abgefüllt

--- UI/FahrzeugDatenUI.xaml.cs
+++ UI/FahrzeugDatenUI.xaml.cs
@@ -29,6 +29,26 @@ namespace MDP_Projekt.UI
             this.comboFahrzeugArt.ItemsSource = LoadFahrzeugartCombo();
             this.comboFahrzeugArt.DisplayMemberPath = "FZA_BEZEICHNUNG";
             this.comboFahrzeugArt.SelectedValuePath = "FZA_ID";
+            this.comboFahrzeugArt.SelectedValue = 1;
+
+            // ComboBox Fahrzeugmarke abfüllen
+            this.comboFahrzeugMarke.ItemsSource = LoadFahrzeugmarkeCombo();
+            this.comboFahrzeugMarke.DisplayMemberPath = "FZM_BEZEICHNUNG";
+            this.comboFahrzeugMarke.SelectedValuePath = "FZM_ID";
+            this.comboFahrzeugMarke.SelectedValue = 1;
+
+            // ComboBox Fahrzeugtyp abfüllen
+            this.comboFahrzeugTyp.ItemsSource = LoadFahrzeugtypCombo();
+            this.comboFahrzeugTyp.DisplayMemberPath = "FZT_BEZEICHNUNG";
+            this.comboFahrzeugTyp.SelectedValuePath = "FZT_ID";
+            this.comboFahrzeugTyp.SelectedValue = 1;
+
+            // ComboBox Nutzer abfüllen
+            this.comboFahrzeugNutzer.ItemsSource = LoadFahrzeugnutzerCombo();
+            this.comboFahrzeugNutzer.DisplayMemberPath = "NUT_NAME";
+            this.comboFahrzeugNutzer.SelectedValuePath = "NUT_ID";
+            this.comboFahrzeugNutzer.SelectedValue = 1;
+
         }
 
         private List<Model.T_FAHRZEUG> LoadCollectionData()
@@ -59,5 +79,37 @@ namespace MDP_Projekt.UI
             return arten;
         }
 
+        private List<Model.TZ_FAHRZEUGMARKE> LoadFahrzeugmarkeCombo()
+        {
+            List<Model.TZ_FAHRZEUGMARKE> marken = new List<Model.TZ_FAHRZEUGMARKE>();
+            using (Model.Context context = new Model.Context())
+            {
+                marken = context.TZ_FAHRZEUGMARKE.ToList();
+            }
+
+            return marken;
+        }
+
+        private List<Model.TR_FAHRZEUGTYP> LoadFahrzeugtypCombo()
+        {
+            List<Model.TR_FAHRZEUGTYP> typen = new List<Model.TR_FAHRZEUGTYP>();
+            using (Model.Context context = new Model.Context())
+            {
+                typen = context.TR_FAHRZEUGTYP.ToList();
+            }
+
+            return typen;
+        }
+
+        private List<Model.T_NUTZER> LoadFahrzeugnutzerCombo()
+        {
+            List<Model.T_NUTZER> nutzer = new List<Model.T_NUTZER>();
+            using (Model.Context context = new Model.Context())
+            {
+                nutzer = context.T_NUTZER.ToList();
+            }
+
+            return nutzer;
+        }
     }
 }
